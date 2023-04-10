df163a832f00a8b108e13cf99ae7bba6436df5d3
Matvei Kulakovski <matveikulakovski@gmail.com>
2023-04-10 16:08:15 +0300

    arvutab töötajate arv ja annab nime @Totalcount

--- proceduur.sql
+++ proceduur.sql
@@ -50,7 +50,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+---arvutab töötajate arv ja annab nime @Totalcount
 create proc spTotalCount2
 @TotalCount int output
 as begin
