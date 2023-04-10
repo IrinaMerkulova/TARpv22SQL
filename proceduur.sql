5b18c340735c9c34502fd4c8f6ddd7c363d04887
Timur Denisenko <timur.denisenko.2109@gmail.com>
2023-04-10 15:58:36 +0300

    protkaiv

--- proceduur.sql
+++ proceduur.sql
@@ -34,7 +34,9 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- protseduuri käivitamine,
+-- Juhul kui töötajate arv=0 >> "is null"
+-- Juhul kui ei võtdu null >> "is not null"
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
