b6dd8b6c9a27763d41c8b0b18fc248d05eb56151
Edvard Datser <edvard.datser@icloud.com>
2023-04-10 16:16:43 +0300

    küsib ID, ja näitab Eesnimi ID järgi.

--- proceduur.sql
+++ proceduur.sql
@@ -76,11 +76,11 @@ declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
-create proc spGetNameById2
+--küsib ID, ja näitab Eesnimi ID järgi.
+create proc spGetNameById3
 @Id int
 as begin
-	return (select FirstName from Employees where Id = @Id)
+	select FirstName from Employees where Id = @Id
 end
 
 -- ?
