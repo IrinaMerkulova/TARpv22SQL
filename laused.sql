8fa50dea2d37133612505054c91f95c7fb470d74
Matvei Kulakovski <matveikulakovski@gmail.com>
2023-04-10 13:49:59 +0300

    võõrvõtme kustutamine

--- laused.sql
+++ laused.sql
@@ -48,7 +48,7 @@ values (7, 'Spiderman', 'spider@spiderman.com', 2)
 -- vaatame tabeli andmeid
 select * from Person
 
----?
+---võõrvõtme kustutamine
 alter table Person
 drop constraint tblPerson_GenderId_FK
 
