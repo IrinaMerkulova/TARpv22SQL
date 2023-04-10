1a6da7f162c90fa43ceed5832b28b4c4962d278d
Timur Denisenko <timur.denisenko.2109@gmail.com>
2023-04-10 16:17:58 +0300

    id

--- proceduur.sql
+++ proceduur.sql
@@ -76,7 +76,7 @@ print 'Name of the employee = ' + @FirstName
 create proc spGetNameById2
 @Id int
 as begin
-	return (select FirstName from Employees where Id = @Id)
+	select FirstName from Employees where Id = @Id
 end
 
 -- ?
