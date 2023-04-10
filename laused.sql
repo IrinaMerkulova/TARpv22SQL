5847db8ecd7c900df49eb249dea645e5190ae04b
Matvei Kulakovski <matveikulakovski@gmail.com>
2023-04-10 14:32:47 +0300

    cast - teiseldab int andmetüüpiks

--- laused.sql
+++ laused.sql
@@ -145,6 +145,8 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
+-- cast - teiseldab int andmetüüpiks
+
 --?
 select sum(cast(Age as int)) from Person
 
