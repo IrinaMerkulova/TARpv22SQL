6d980aff4c4d32e342a63a29b83d135d7b8f2567
Darja Miljukova <miljukovadarja@gmail.com>
2023-04-10 14:01:41 +0300

    näitab linnad milles on täht n ja email milles on @

--- laused.sql
+++ laused.sql
@@ -115,7 +115,7 @@ Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
 
 
---- ?
+--- näitab linnad milles on täht "n" ja email milles on "@"
 select * from Person where City like 'n%'
 select * from Person where Email like '%@%'
 
