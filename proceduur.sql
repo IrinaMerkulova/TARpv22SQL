02492717a132c23d947c36fa0f39451b4461de69
Luca Gluhhov <opilane@tthk.ee>
2023-04-10 15:57:33 +0300

    failid on laetud

--- proceduur.sql
+++ proceduur.sql
@@ -26,7 +26,7 @@ spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
---?
+--küsib gender ja arvutab töötajate arvu ID järgi
 create proc spGetEmployeeCountByGender
 @Gender nvarchar(20),
 @EmployeeCount int output
@@ -34,7 +34,7 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- protseduuri käivitamine, Juhul kui töötajate arv=0 >> "is null", juhul kui ei võrdu null >> "is not null
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
