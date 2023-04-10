02e27fdbe18a06c49badf396b3226bb4616621c6
Timur Denisenko <timur.denisenko.2109@gmail.com>
2023-04-10 16:13:54 +0300

    id

--- proceduur.sql
+++ proceduur.sql
@@ -59,7 +59,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- küsib id; ja näiteb Eesnimi ID järgi
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
