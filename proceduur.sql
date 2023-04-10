86eda95bd0790f8fc137c839db85d3c399a5f7e3
Aleksander Rogovski <aleks09.teki5@gmail.com>
2023-04-10 16:19:10 +0300

    final

--- proceduur.sql
+++ proceduur.sql
@@ -26,7 +26,7 @@ spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
----
+---Küsib gender ja arvutab töötajate arvu ID järgi
 create proc spGetEmployeeCountByGender
 @Gender nvarchar(20),
 @EmployeeCount int output
@@ -34,7 +34,9 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- protseduuri käivitamine
+---Juhul kui töötajate arv=0 >> "is null"
+---Juhul kui ei võrgu null >> "is not null"
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
@@ -48,7 +50,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+---arvutab töötajate arv ja annab nime @totalcount
 create proc spTotalCount2
 @TotalCount int output
 as begin
@@ -59,7 +61,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- Küsib Id, ja näitab Eesnimi ID järgi
 create proc spGetNameById1
 @Id int,
 @Name nvarchar(50) output
@@ -67,21 +69,21 @@ as begin
 	select @Name = Name from employees where Id = @Id
 end
 
---?
-declare @FirstName nvarchar(50)
+--Protseduuri käivitamine
+declare @Name nvarchar(50)
 execute spGetNameById1 6, @Name output
 print 'Name of the employee = ' + @Name
 
---?
-create proc spGetNameById2
+--Küsib Id, ja näitab Eesnimi ID järgi
+create proc spGetNameById3
 @Id int
 as begin
-	return (select Name from Employees where Id = @Id)
+	select Name from Employees where Id = @Id
 end
 
--- ?
+-- prots käevetamine
 declare @EmployeeName nvarchar(50)
-exec @EmployeeName = spGetNameById2 1
+exec @EmployeeName = spGetNameById3 1
 print 'Name of the employee = ' + @EmployeeName
 
 select * from Employees
