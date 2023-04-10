2ace226a6a52380024f9a9ac179dfc4f2f196ee2
Matvei Kulakovski <matveikulakovski@gmail.com>
2023-04-10 14:22:02 +0300

    näitab, kas meilidel pole @-märki

--- laused.sql
+++ laused.sql
@@ -119,7 +119,7 @@ select * from Person where Age in (100, 50, 20)
 select * from Person where City like 'n%'
 select * from Person where Email like '%@%'
 
--- ?
+-- näitab, kas meilidel pole @-märki
 select * from Person where Email not like '%@%'
 
 --- näitab, kelle on emailis ees ja peale @-märki
