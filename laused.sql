80721fa9fad2a8ee03fe29794e512d15336e3831
Timur Denisenko <timur.denisenko.2109@gmail.com>
2023-04-10 14:27:28 +0300

    algab n

--- laused.sql
+++ laused.sql
@@ -115,7 +115,7 @@ Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
 
 
---- ?
+--- Näitab linnu, mis algavad tähega "n" ja näitab kirja, mis sisaldab "@"
 select * from Person where City like 'n%'
 select * from Person where Email like '%@%'
 
