657926e3926e35dccb11cd3d185688081716d128
Maksim Tsepelevits <maksimplamja@gmail.com>
2023-04-10 14:32:35 +0300

    See käsk teeb päringu tabelist Töötajad ja tagastab unikaalsed veeru Name ja DepartmentId väärtuste paarid

--- laused.sql
+++ laused.sql
@@ -210,7 +210,7 @@ values (10, 'Russell', 'Male', 8800, NULL)
 
 select * from Employees
 
----?
+-- See käsk teeb päringu tabelist "Töötajad" ja tagastab unikaalsed veeru "Name" ja "DepartmentId" väärtuste paarid.
 select distinct Name, DepartmentId from Employees
 
 ---?
