eb4c1a111f7cbe5afeb2aa3947a7414a83f717d0
Artur Shushkevitch <artursuskevits@gmail.com>
2023-04-10 15:27:32 +0300

    ultralast

--- laused.sql
+++ laused.sql
@@ -160,7 +160,7 @@ select City, sum(cast(Age as int)) as TotalAge from Person group by City
 
 
 --- tund 3
-
+drop table Employees
 --- loome uued tabelid
 create table Department
 (
@@ -173,7 +173,7 @@ DepartmentHead nvarchar(50)
 create table Employees
 (
 Id int primary key,
-Name nvarchar(50),
+FirstName nvarchar(50),
 Gender nvarchar(10),
 Salary nvarchar(50),
 DepartmentId int
@@ -191,25 +191,25 @@ values (4, 'Other Deparment', 'Sydney', 'Cindrella')
 
 select * from Department
 
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (1, 'Tom', 'Male', 4000, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (2, 'Pam', 'Female', 3000, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (3, 'John', 'Male', 3500, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (4, 'Sam', 'Male', 4500, 2)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (5, 'Todd', 'Male', 2800, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id,FirstName, Gender, Salary, DepartmentId)
 values (6, 'Ben', 'Male', 7000, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (7, 'Sara', 'Female', 4800, 3)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (8, 'Valarie', 'Female', 5500, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (9, 'James', 'Male', 6500, NULL)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+insert into Employees (Id, FirstName, Gender, Salary, DepartmentId)
 values (10, 'Russell', 'Male', 8800, NULL)
 
 select * from Employees
