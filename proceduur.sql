fb8dabd8c9ec4d6acaf5066a96463243b58ad2e8
Aleksandr nadvornoi <nadvornyjaleksandr74@gmail.com>
2023-04-10 16:19:18 +0300

    prottseduri

--- proceduur.sql
+++ proceduur.sql
@@ -34,7 +34,9 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- prottseduri kävitamine,
+-- juhul kui töötajale arv=0 >> is null
+-- juhul kui ri võtdu null >> is not null
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
@@ -48,7 +50,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+--- arvutab töötajate arv ja annab @Totalcount
 create proc spTotalCount2
 @TotalCount int output
 as begin
@@ -59,7 +61,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- küsib id, ja mäitab eesnimi ID järgi
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
@@ -67,21 +69,21 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
---?
+--proc kävitamine 
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
+-- küsib id ja eesnimi järgi
 create proc spGetNameById2
 @Id int
 as begin
-	return (select FirstName from Employees where Id = @Id)
+	select  FirstName from Employees where Id = @Id)
 end
 
--- ?
+-- 
 declare @EmployeeName nvarchar(50)
-exec @EmployeeName = spGetNameById2 1
+exec @EmployeeName = spGetNameById3 1
 print 'Name of the employee = ' + @EmployeeName
 
 select * from Employees
