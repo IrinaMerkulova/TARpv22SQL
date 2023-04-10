ba9ec6edc923d84a2e118707f3a04c6c82288ef2
Veronika Milovzorova <veronika.milovzorova@gmail.com>
2023-04-10 16:05:21 +0300

    arvutab töötajate arv ja annab nime @totalcount

--- proceduur.sql
+++ proceduur.sql
@@ -5,12 +5,12 @@ create procedure spGetEmployees
 as begin
 	select FirstName, Gender from Employees
 end
-
+--protseduuri käivitamine
 spGetEmployees
 exec spGetEmployees
 execute spGetEmployees
 
---- 
+---küsib gender ja arvutab töötajate arvu ID järgi
 create proc spGetEmployeesByGenderAndDepartment
 @Gender nvarchar(20),
 @DepartmentId int
@@ -26,7 +26,7 @@ spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
---?
+--küsib Gender ja DepartamentID ning kuvab andmet tabelist
 create proc spGetEmployeeCountByGender
 @Gender nvarchar(20),
 @EmployeeCount int output
@@ -34,7 +34,9 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- protseduuri käivitamine
+--juhul kui töötajate arv=0 >> "is null"
+--juhul kui ei võitdu nul >> "is not null"
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
@@ -48,7 +50,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+---arvutab töötajate arv ja annab nime @totalcount
 create proc spTotalCount2
 @TotalCount int output
 as begin
