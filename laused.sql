78a2476d1d091ccc0a5f3bdbc88f071c5cd614a5
Maksim Tsepelevits <maksimplamja@gmail.com>
2023-04-10 14:30:10 +0300

    Lisab andmed tabelisse Department ja Employees

--- laused.sql
+++ laused.sql
@@ -175,7 +175,7 @@ Salary nvarchar(50),
 DepartmentId int
 )
 
--- Lisab andmed tabelisse Department
+-- Lisab andmed tabelisse Department ja Employees
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
 values (1, 'IT', 'London', 'Rick')
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
