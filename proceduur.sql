d7f3399535379f147e922096939f733db996cd3e
Darja Miljukova <miljukovadarja@gmail.com>
2023-04-10 16:06:11 +0300

    arvutab töötajate arv ja annab nime @Totalcount

--- proceduur.sql
+++ proceduur.sql
@@ -34,7 +34,9 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- protseduuri käivitamin, 
+-- juhul kui töötajate arv=0 >> "is null"
+-- juhul kui ei võrdu null >>  "is not null"
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
@@ -48,7 +50,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+---arvutab töötajate arv ja annab nime @Totalcount
 create proc spTotalCount2
 @TotalCount int output
 as begin
