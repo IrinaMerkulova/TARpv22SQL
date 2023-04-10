7cbabe4e1a031c1f560ab83715d63d16835ac9cb
Deniss Gorjunov <pinkod2222@gmail.com>
2023-04-10 14:20:46 +0300

    näitab kõik nimed mis ei alga w või A või S

--- laused.sql
+++ laused.sql
@@ -126,7 +126,7 @@ select * from Person where Email not like '%@%'
 -- ainult üks täht
 select * from Person where Email like '_@_.com'
 
---?
+--näitab kõik nimed mis ei alga w või A või S
 select * from Person where Name like '[^WAS]%'
 --- ?
 select * from Person where (City = 'Gotham' or City = 'New York')
